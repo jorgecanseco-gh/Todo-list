cc36ccfdf13339fa1abce9590e3c000b132d7728
Jorge.C <jorgecansecoreal@gmail.com>
2025-04-08 11:35:40 +0200

    functional todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import React, { useState } from 'react'; // Importing React and useState from react
 
 function App() {
+    const [inputValue, setInputValue] = useState(''); // UseState for input value
+    const [tasks, setTasks] = useState([]); // USestate for task LIst
+  
+  const handleAddTask = () => {
+    if (inputValue.trim() === '') {
+      return; // Prevent adding empty tasks
+    } else {
+    setTasks([...tasks, inputValue]); // Add task to the list
+    setInputValue(''); // Clear the input field
+  }
+  };
+
+  const handleDeleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index)); // Delete task from the list
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+    <h1 className={styles.title}>TODO LIST</h1>
+    <section className={styles.addTask}>
+    <div className={styles.inputContainer}>
+      <input 
+      type='text' 
+      className={styles.addTaskInput} 
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+      ></input>
+    </div>
+      <button className={styles.addTaskButton} onClick={handleAddTask}>Add</button>
+    </section>
+    <section className={styles.taskList}>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task} 
+            <span onClick={() => handleDeleteTask(index)}>❌</span>
+          </li>
+        ))}
+      </ul>
+    </section>
     </div>
   );
 }
